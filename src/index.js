6ff0a0dbd580e3f198fa585229290d58fc9801bd
CarmenMerino <carmenmerino2202@gmail.com>
2022-02-21 10:05:59 -0500

    terminado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import validator from "./validator.js";
 let txtTarjeta = document.getElementById("txtTarjeta");
+let pTarjeta = document.getElementById("pTarjeta");
 let btnValidar = document.getElementById("btnValidar");
 btnValidar.addEventListener("click", validarTarjeta);
 function validarTarjeta() {
   let numeroTarjeta = txtTarjeta.value;
-  console.log("el numero a verificar es", numeroTarjeta)
+  //console.log("el numero a verificar es", numeroTarjeta)
   let esValido = validator.isValid(numeroTarjeta);
   if (esValido == true) {
     document.getElementById("pagina1").className = "oculto";
@@ -16,7 +17,7 @@ function validarTarjeta() {
 }
 function enmascarar() {
   let numeroTarjeta = txtTarjeta.value;
-  console.log("el numero a enmascarar es", numeroTarjeta)
+  //console.log("el numero a enmascarar es", numeroTarjeta)
   let mascara = validator.maskify(numeroTarjeta);
   let resultadoTarjeta = document.getElementById("resultadoTarjeta1")
   resultadoTarjeta.innerHTML=mascara
@@ -30,3 +31,17 @@ function enmascarar2() {
   resultadoTarjeta.innerHTML=mascara
 }
 btnValidar.addEventListener("click", enmascarar2);
+
+txtTarjeta.addEventListener('input',cambioTarjeta)
+
+function cambioTarjeta(eventoInput) {
+  pTarjeta.textContent = eventoInput.target.value;
+  //console.log("el numero es ", eventoInput.target.value);
+}
+
+function regresarPagina(){
+  document.getElementById("pagina3").className = "oculto";
+  document.getElementById("pagina1").className = "visible";
+}
+var btnRegresar = document.getElementById("btnRegresar");
+btnRegresar.addEventListener("click",regresarPagina);
